ea0e51bc4d94b03a1d4abdfffb19d592cba620e5
Bruno Erick <141072611+bericktk@users.noreply.github.com>
2024-11-13 23:38:32 -0300

    Add files via upload

--- workout/atleta/models.py
+++ workout/atleta/models.py
@@ -1,9 +1,10 @@
 from datetime import datetime
-from sqlalchemy import DateTime,Integer, String, Float
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import DateTime, ForeignKey,Integer, String, Float
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from workout.categorias.models import CategoriaModel
 from workout.contrib.models import BaseModel
 
-class AtletaModel():
+class AtletaModel(BaseModel):
     __tablename__ = 'atletas'
 
     pk_id: Mapped[int] = mapped_column(Integer, primary_key=True)
@@ -14,3 +15,8 @@ class AtletaModel():
     altura: Mapped[float] = mapped_column(Float, nullable=False)
     sexo: Mapped[str] = mapped_column(String(1), nullable=False)
     created_at: Mapped[datetime] = mapped_column(DateTime, nullable=False)
+    categoria: Mapped['CategoriaModel'] = relationship(back_populates='atleta')
+    categoria_id = Mapped[int] = mapped_column(ForeignKey('categorias.pk_id'))
+    centro_treinamento: Mapped['CentroTreinamentoModel'] = relationship(back_populates='atleta')
+    categoria_id = Mapped[int] = mapped_column(ForeignKey('centro_treinamento.pk_id'))
+    
--- workout/atleta/schemas.py
+++ workout/atleta/schemas.py
@@ -1,12 +1,11 @@
 from typing import Annotated
-from pydantic import BaseModel, Field, PositiveFloat
+from pydantic import Field, PositiveFloat
+from workout.contrib.schemas import BaseSchema
 
-class Atleta(BaseModel):
-    nome: Annotated[str, Field(description='Nome do atleta', examples='Bruno Erick', max_length=50)]
-    cpf: Annotated[str, Field(description='CPF do atleta', examples='12345678900', max_length=11)]
-    idade: Annotated[int, Field(description='Idade do atleta', examples=25)]
-    peso: Annotated[PositiveFloat, Field(description='Peso do atleta', examples=75.5)]
-    altura: Annotated[PositiveFloat, Field(description='Altura do atleta', examples=85.3)]
-    sexo: Annotated[str, Field(description='Sexo do atleta', examples='M', max_length=1)]
-    
-    
+class Atleta(BaseSchema):
+    nome: Annotated[str, Field(description='Nome do atleta', example='Bruno Erick', max_length=50)]
+    cpf: Annotated[str, Field(description='CPF do atleta', example='12345678900', max_length=11)]
+    idade: Annotated[int, Field(description='Idade do atleta', example=25)]
+    peso: Annotated[PositiveFloat, Field(description='Peso do atleta', example=75.5)]
+    altura: Annotated[PositiveFloat, Field(description='Altura do atleta', example=85.3)]
+    sexo: Annotated[str, Field(description='Sexo do atleta', example='M', max_length=1)]
